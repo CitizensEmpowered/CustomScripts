37b110d391f3cd53889ea580c3de9d9ed4d1c617
Bryan Jensen <bryanawjensen@gmail.com>
2015-08-23 21:06:20 -0700

    Setting primitive delay

--- js/representative.js
+++ js/representative.js
@@ -1,9 +1,18 @@
 $(function() {
     var $repInfoContainer = $('.representative-info');
 
+    function showRepInfo() {
+        if (!signedInUserInfo) {
+            setTimeout(showRepInfo, 500);
+        }
+        else {
+            $.get('https://congress.api.sunlightfoundation.com/legislators/locate?zip=' + signedInUserInfo.zip + '&apikey=dd1efa0eb1134301ab43db5864ba5e78', function(result) {
+                console.log(result);
+            });
+        }
+    }
+
     if ($repInfoContainer.length) {
-        $.get('https://congress.api.sunlightfoundation.com/legislators/locate?zip=' + signedInUserInfo.zip + '&apikey=dd1efa0eb1134301ab43db5864ba5e78', function(result) {
-            console.log(result);
-        });
+        showRepInfo();
     }
 });
