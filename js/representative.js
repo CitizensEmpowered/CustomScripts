e5980dbbf9f5359d596f2f4953653eb73c7c6989
Bryan Jensen <bryanawjensen@gmail.com>
2015-08-23 21:12:09 -0700

    Showing basic info

--- js/representative.js
+++ js/representative.js
@@ -7,7 +7,15 @@ $(function() {
         }
         else {
             $.get('https://congress.api.sunlightfoundation.com/legislators/locate?zip=' + signedInUserInfo.zip + '&apikey=dd1efa0eb1134301ab43db5864ba5e78', function(result) {
-                console.log(result);
+                result.results.forEach(function(rep) {
+                    var $div = $('<div>');
+                    $div.append($('<span>', { html: rep.first_name }));
+                    $div.append($('<span>', { html: rep.middle_name }));
+                    $div.append($('<span>', { html: rep.last_name }));
+                    $div.append($('<span>', { html: '(' + rep.party + ')' }));
+                    $div.append($('<a>', { text: 'contact', href: rep.contact_form }));
+                    $repInfoContainer.append($div);
+                });
             });
         }
     }
